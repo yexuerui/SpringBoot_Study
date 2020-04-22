b77a7f4aa709608cf81c050978739ddabfc3ed96
yexuerui <yexuerui@xdf.cn>
2020-04-22 13:00:51 +0800

    测试reset命令

--- src/main/java/com/git/Git1.java
+++ src/main/java/com/git/Git1.java
@@ -1,8 +1,8 @@
 package com.git;
 
 /**
- *
  * 增加git文件
+ *
  * @author by yexuerui@xdf.cn
  * @Date 2020-04-21 22:34
  */
@@ -11,6 +11,14 @@ public class Git1 {
 
     public static void main(String[] args) {
         System.out.println("hello git!");
+    }
+
+
+    /**
+     * 小A提交的代码
+     */
+    public void tet() {
+
     }
 
 
